de97f260f33cf36e4ab125ba0d17455e03a01a96
jdluis <jdluisdev@gmail.com>
2022-06-25 21:13:29 +0100

    Revert "Backup of Backend"
    This reverts commit 0553718b29bab7b6b938ec9159dd5fb6800a1296.

--- server/index.ts
+++ server/index.ts
@@ -4,14 +4,3 @@ import { startConnection } from "./database";
 startConnection();
 app.listen(3000);
 console.log('Server is running on port 3000');
-
-/*
-|--------------------------------------------------------------------------
-| BackEnd Info Branch
-|--------------------------------------------------------------------------
-|
-| In this Branch i save a backup of the backEnd
-| for check if i want to create a new proyect or
-| if a have some problems in the future.
-|
-*/
