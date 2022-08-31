d5e6639bd45ba541cb6f83e747d1e420a739a426
jdluis <jdluisdev@gmail.com>
2022-08-31 21:38:51 +0100

    refac and add .env

--- server/app.ts
+++ server/app.ts
@@ -2,13 +2,12 @@ import express from 'express'
 import morgan from 'morgan';
 import cors from 'cors';
 import path from "path";
-
-import taskRoutes from './routes' //EN ts y js con node, no hace falta definir que hay un index
+import taskRoutes from './routes' 
 
 const app = express()
 
 app.use(cors());
-app.use(morgan('dev')); //nos refleja en consola todo lo que pidamos en el lado del servidor
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api', taskRoutes)
